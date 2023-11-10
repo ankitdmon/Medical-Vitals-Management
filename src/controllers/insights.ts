22eb66581b62a8443722b79f04100c894da916fa
Ankit-Flutch <ankitmishra2108@gmail.com>
2023-11-10 15:23:41 +0530

    Aggregation & Insights...

--- src/controllers/insights.ts
+++ src/controllers/insights.ts
@@ -5,36 +5,34 @@ import {
 } from "../helper/response";
 import { Request, Response } from "express";
 import { CommandType } from "../validations/validation";
-import { VitalModel } from "../models/vital";
+import {
+  calculatePercentile,
+  getPopulationValues,
+  vitalValue,
+} from "../services/aggregate";
+
 
 export const aggregateVitals = async (req: Request, res: Response) => {
   try {
     const { command, username, vital_ids, start_timestamp, end_timestamp } =
       req.body;
+
     if (command === CommandType.AGGREGATE) {
-      const aggregates: { [key: string]: number } = {};
-      for (const vital_id of vital_ids) {
-        const result = await VitalModel.aggregate([
-          {
-            $match: {
-              userName: username,
-              vitalID: vital_id,
-              timestamp: {
-                $gte: new Date(start_timestamp),
-                $lte: new Date(end_timestamp),
-              },
-              deletedAt: null,
-            },
-          },
-          {
-            $group: {
-              _id: null,
-              average: { $avg: "$value" },
-            },
-          },
-        ]);
-        aggregates[vital_id] = result.length ? result[0].average : 0;
-      }
+      const aggregatePromises = vital_ids.map(async (vital_id: string) => {
+        const average = await vitalValue(
+          username,
+          vital_id,
+          start_timestamp,
+          end_timestamp
+        );
+        return { [vital_id]: average };
+      });
+
+      const aggregates = Object.assign(
+        {},
+        ...(await Promise.all(aggregatePromises))
+      );
+
       return successResponse(req, res, {
         username,
         aggregates,
@@ -51,6 +49,34 @@ export const aggregateVitals = async (req: Request, res: Response) => {
 
 export const populationInsight = async (req: Request, res: Response) => {
   try {
+    const { command, username, vital_id, start_timestamp, end_timestamp } =
+      req.body;
+
+    if (command !== CommandType.POPULATION_INSIGHT) {
+      return failResponse(req, res, `Invalid command type ${command}`);
+    }
+
+    const populationValues = await getPopulationValues(
+      vital_id,
+      start_timestamp,
+      end_timestamp
+    );
+    const userAverage = await vitalValue(
+      username,
+      vital_id,
+      start_timestamp,
+      end_timestamp
+    );
+
+    const percentile = calculatePercentile(userAverage, populationValues);
+
+    return successResponse(req, res, {
+      username,
+      vital_id,
+      start_timestamp,
+      end_timestamp,
+      insight: `Your ${vital_id} is in the ${percentile}th percentile.`,
+    });
   } catch (err) {
     return errorResponse(req, res, err as Error);
   }
---
+++ src/services/aggregate.ts
@@ -0,0 +1,49 @@
+import { VitalModel } from "../models/vital";
+
+export const calculatePercentile = (
+  userValue: number,
+  populationValues: number[]
+): number => {
+  const sortedValues = populationValues.sort((a, b) => a - b);
+  const countBelowOrEqual = sortedValues.filter(
+    (value) => value <= userValue
+  ).length;
+  const percentile = (countBelowOrEqual / sortedValues.length) * 100;
+  return percentile;
+};
+
+export const vitalValue = async (
+  username: string,
+  vital_id: string,
+  start_timestamp: Date,
+  end_timestamp: Date
+) => {
+  const vitals = await VitalModel.aggregate([
+    {
+      $match: {
+        userName: username,
+        vitalID: vital_id,
+        timestamp: {
+          $gte: new Date(start_timestamp),
+          $lte: new Date(end_timestamp),
+        },
+        deletedAt: null,
+      },
+    },
+    {
+      $group: {
+        _id: null,
+        average: { $avg: "$value" },
+      },
+    },
+  ]);
+  return vitals.length ? vitals[0].average : 0;
+};
+
+export const getPopulationValues = async (vital_id: string, start_timestamp: string, end_timestamp: string) => {
+    return (await VitalModel.find({
+        vitalID: vital_id,
+        timestamp: { $gte: new Date(start_timestamp), $lte: new Date(end_timestamp) },
+        deletedAt: null,
+    })).map(value => value.value);
+  };
